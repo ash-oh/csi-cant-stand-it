f1deae9e0adb6881d29ecd7f66f9398c07d292d7
Ashley Oh <ohas@bc.edu>
2019-11-25 23:35:59 -0500

    raw data

--- code/scraping.py
+++ code/scraping.py
@@ -5,24 +5,25 @@ from soupsieve import select
 episodes = {}
 
 for i in range(1,3):
-    print ("/npage/n")
+  
     page = open("data/transcripts/page-" + str(i) + ".html", 'r')
-    soup = BeautifulSoup(page.read())
-
-    for row in select(soup, "td.topic-titles a"):
-        parts = row.text.split(" - ")
-        episodes[parts[0]] = {"title": parts[1], "link": row.get("href")}
+    soup = BeautifulSoup(page.read(), features="lxml")
+    #print(soup.prettify())
+    #print (soup.select(".topic-titles.row2 h3 a"))
+    doc = soup.select(".topic-titles.row2 h3 a")
+    for row in doc:
+        if row != doc[0] and row != doc[1]: 
+            parts = row.text.split(" - ")
+            episodes[parts[0]] = {"title": parts[1], "link": row.get("href")}
 
 for key, value in episodes.items():
     parts = key.split("x")
     season = int(parts[0])
     episode = int(parts[1])
     filename = "data/transcripts/S%d-Ep%d" %(season, episode)
-    print (filename)
-
     with open(filename, 'wb') as handle:
-        headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-        response = requests.get("http://transcripts.foreverdreaming.org" + value["link"], headers = headers)
+        headers = {'User-Agent': 'Chrome/78.0.3904.106'}
+        response = requests.get(value["link"],headers = headers)
         if response.ok:
             for block in response.iter_content(1024):
                 if not block:
