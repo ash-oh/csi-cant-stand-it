9dbabe582209322e375ed81fcfc462180eb1e66f
Ashley Oh <ohas@bc.edu>
2019-12-02 22:15:07 -0500

    sorting function

--- code/sort.py
+++ code/sort.py
@@ -2,7 +2,8 @@ import os
 murderers = ["husband", "Jerrod Cooper", "Jamie Smith", "James Moore", "Kyle Travis", "Winston Barger", "Bobby", "Julia Easton", "Kit Armstrong", "Amy Hendler", "Jesse Overton", "Tina Collins"
     , "Nate Metz","Lou Everett" ,"Marlene Valdez", "Max Valdez",  "Dr. Kiera Berhle", "Scott Shelton", "Female Trucker", "Girlfriend", "Kenny Berlin", "Teller 12"
     ,"Paul Millander","Julia Barret","Stu Evans","Roger Jennings","Cameron Nelson","Officer Spencer","Marty Gilmore","Claudia Gideon","Mickey Rutledge","Paul Millander"
-    ,"Roy Logan","Russ Bradley","Bartender" ,"Doctor" ,"Jimmy Tadero"]
+    ,"Roy Logan","Russ Bradley","Bartender" ,"Doctor" ,"Jimmy Tadero","Jack","Luke","Patrick Haynes","Amanda Haynes","Tony Thorpe","Roy McCall","Tammy Felton","Justin Green","Mark Rucker","Brad Kendal","Susan Hillridge","Carla Dantini"
+    ,"Fred Applewhite","Brad Walden","Syd Goggle","Bonnie Ritten","Walt Braun","Mrs Buckley", "Leigh Spaien", "Ian Wolf"]
 files = []
 for name in murderers:
     name = name.replace(" ","_")
@@ -10,11 +11,13 @@ for name in murderers:
     name = name + ".txt"
     files.append(name)
 
-
+os.mkdir("data/good")
+os.mkdir("data/bad")
 for filename in os.listdir("data/character"):
     path = "data/character/"+filename
+    print (path)
     if filename in files:
-        os.rename("data/character/"+filename, "data/character/bad/"+ filename)
+        os.rename("data/character/"+filename, "data/bad/"+ filename)
     else:
-        os.rename("data/character/"+filename, "data/character/good/"+ filename)
+        os.rename("data/character/"+filename, "data/good/"+ filename)
     
