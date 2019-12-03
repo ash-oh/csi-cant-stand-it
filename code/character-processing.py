a00af2bef469f2c7d64a9e52facdb6a54259c292
Ashley Oh <ohas@bc.edu>
2019-12-02 20:23:54 -0500

    extracting characters from scripts

--- code/character-processing.py
+++ code/character-processing.py
@@ -1,28 +1,32 @@
 import os
 
 for file in os.listdir("data/transcripts"):
-	path = "data/transcripts/" + file
-	input_file = open(path)
+    path = "data/transcripts/" + file
+    print(file)
+    input_file = open(path)
+#not the best method of extracting characters because will split in the middle of lines if there is :
+#if time will explore again using regex
+    for line in input_file:
+        if ':' in line:
+            name = line.split(':')[0]
+            if any(c.islower() for c in name) and ('Sound' not in name) and ('(' not in name):
+                filename = name.strip()
+                filename = filename.lower()
+                filename = filename.replace(" ","_")
+                filename = filename.replace("/","-")
 
-	for line in input_file:
-		if ':' in line:
-			name = line.split(':')[0]
-			if any(c.islower() for c in name) and ('Sound' not in name) and ('(' not in name):
-				filename = name.strip()
-				filename = filename.replace(" ","_")
+                name = name + ':'
+                name = name.lstrip()
 
-				name = name + ':'
-				name = name.lstrip()
+                output_file = open("data/character/"+filename + ".txt", 'a+')
 
-				output_file = open("data/character/"+filename + ".txt", 'a+')
-				
-				line = line.replace(name, '')
-				line = line.replace("&quot;", "")
-				line = line.lstrip()
-				output_file.write(line)
+                line = line.replace(name, '')
+                line = line.replace("&quot;", "")
+                line = line.lstrip()
+                output_file.write(line)
 
-	input_file.close()
-	output_file.close()
+    input_file.close()
+    output_file.close()
 
 
 
