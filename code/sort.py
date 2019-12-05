011173a1699849ed2d8fe20b6b49abead1abcebe
Ashley Oh <ohas@bc.edu>
2019-12-04 21:31:17 -0500

    da

--- code/sort.py
+++ code/sort.py
@@ -5,9 +5,64 @@ murderers = ["husband", "Jerrod Cooper", "Jamie Smith", "James Moore", "Kyle Tra
     ,"Roy Logan","Russ Bradley","Bartender" ,"Doctor" ,"Jimmy Tadero","Jack","Luke","Patrick Haynes","Amanda Haynes","Tony Thorpe","Roy McCall","Tammy Felton","Justin Green","Mark Rucker","Brad Kendal","Susan Hillridge","Carla Dantini"
     ,"Fred Applewhite","Brad Walden","Syd Goggle","Bonnie Ritten","Walt Braun","Mrs Buckley", "Leigh Spaien", "Ian Wolf"
     ,"Sean Nolan", "Nigel Crane", "Gordon Daimler", "Nicole Paggi"]
-
+serial = ["Michelle Baldwin",
+"Arthur Blisterman",
+"Jared Briscoe",
+"Jenny Carroll",
+"Shane Casey",
+"CASt",
+"Tom Cooley",
+"Gordon Daimler",
+"Henry Darius",
+"Natalie Davis",
+"Steve Davis",
+"Charlie DiMasa",
+"Clay Dobson",
+"Thomas Donover",
+"Hollis Eckhart",
+"Esteban Fellipe",
+"Tammy Felton",
+"Darin Hanson",
+"Stuart Gardner",
+"Lou Gedda",
+"Syd Booth Goggle",
+"Kevin Greer",
+"Nate Haskell",
+"Catherine Hill",
+"Susan Hillridge",
+"John Himmel",
+"Dominik Janos",
+"Paul Kimball",
+"David Lowry",
+"Jason McCann",
+"Frank McCarthy",
+"Paul Millander",
+"Kip Miller",
+"Esteban Navarro",
+"Stewart Otis",
+"Marty Pino",
+"Daniel Pritchard",
+"Walter Resden",
+"Bill Ryan",
+"Dimitri Sadesky",
+"Alisa Santiago",
+"Gina Sinclair",
+"Casey Steele",
+"Tyler Stirling",
+"Suspect X",
+"Matthew Tarland",
+"Jack Toller",
+"Sam Vega",
+"Lucas Wade",
+"Ellen Whitebridge",
+"Terry Lee Wicker",
+"David Wilson",
+"Paul Winthrop",
+"Jake Tarland",
+"Ellie Brassh",
+"Matthew Tarland"]
 files = []
-for name in murderers:
+for name in serial:
     name = name.replace(" ","_")
     name = name.lower()
     name = name + ".txt"
@@ -19,7 +74,7 @@ for filename in os.listdir("data/character"):
     path = "data/character/"+filename
     print (path)
     if filename in files:
-        os.rename("data/character/"+filename, "data/bad/"+ filename)
-    else:
-        os.rename("data/character/"+filename, "data/good/"+ filename)
+        os.rename("data/character/"+filename, "data/serial/"+ filename)
+    #else:
+     #   os.rename("data/character/"+filename, "data/good/"+ filename)
     
