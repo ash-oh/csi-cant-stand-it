ec2f6ea5ba57f7f35a7221b364efbd1c3641ee83
David Kim <kimbyi@bc.edu>
2019-12-08 17:13:11 -0500

    Add files via upload
    updated ver

--- code/draft.py
+++ code/draft.py
@@ -3,36 +3,63 @@ import nltk
 import csv
 import numpy as np
 from textblob import TextBlob
-
+import os
 
 
 #GLOBAL STUFF
 
 print("[polarity, subjectivity, valence, arousal, dominance]")
-
-
 with open('Ratings_Warriner_et_al.csv', mode='r') as csv_file:
 	csv_reader = csv.reader(csv_file)
 	line_count = 0
 	d = {}
 	for row in csv_reader:
 		d[row[1]] = [row[2],row[5],row[8]] #valence, arousal, dominance in order. 
+gooddata = []
+baddata = []
 
-
-sample = []
-data = []
 
 #functions
 def warriner_rating(alltokens):
 	count = 0
-	ratings = [0,0,0]
+	ratings = []
+	maxValence = 0
+	maxArousal = 0
+	maxDominance = 0
+	minValence = 10
+	minArousal = 10
+	minDominance = 10
 
 	for word in alltokens:
 		if word in d.keys():
-			count += 1
 			newrating = [float(i) for i in d[word]] #['7','2','1'] --> [7,2,1]
-			ratings = [sum(x) for x in zip(ratings,newrating)] #elementwise addition.
-	ratings =[x/count for x in ratings]
+
+			if newrating[0] > maxValence:
+				maxValence = newrating[0]
+			if newrating[1] > maxArousal:
+				maxArousal = newrating[1]
+			if newrating[2] > maxDominance:
+				maxDominance = newrating[2]
+
+			if newrating[0] < minValence:
+				minValence = newrating[0]
+			if newrating[1] < minArousal:
+				minArousal = newrating[1]
+			if newrating[2] < minDominance:
+				minDominance = newrating[2]
+
+			count += 1
+			#ratings = [sum(x) for x in zip(ratings,newrating)] #elementwise addition.
+
+	#ratings =[x/count for x in ratings]
+
+	ratings.append(maxValence)
+	ratings.append(maxArousal)
+	ratings.append(maxDominance)
+	ratings.append(minValence)
+	ratings.append(minArousal)
+	ratings.append(minDominance)
+
 	return ratings
 
 def textblob_sentiment(text):
@@ -41,28 +68,193 @@ def textblob_sentiment(text):
 
 
 # SHOULD BE IN FOR LOOP LATER TO LOOP THORUGH ALL SCRIPT
-f = open("Husband.txt")
-text = f.read().rstrip()
-text = re.sub("\n", " ", text)
 
-text = text.lower()
 
-#textblob
-sentiment = textblob_sentiment(text)
-sample.append(sentiment[0])
-sample.append(sentiment[1])
 
-alltokens = nltk.word_tokenize(text)
-ratings = warriner_rating(alltokens)
+def analyze_script(goodorbad, trainingdata):
+	directory = "/Users/david/Desktop/csi/data/olddata/character/"
+	directory += goodorbad
+	folder= os.fsencode(directory)
+	
+	for file in os.listdir(folder):
+	    filename = directory + "/" + os.fsdecode(file)
+	    f = open(filename)
 
-sample.append(ratings[0])
-sample.append(ratings[1])
-sample.append(ratings[2])
+	    text = f.read().rstrip()
+	    text = text.lower()
 
-print(sample)
+	    firstsample = []
+	    secondsample = []
+	    listoftest = text.split("\n")
+	    secondhalf = "\n".join(listoftest[(len(listoftest)//2):])
+	    firsthalf = "\n".join(listoftest[:(len(listoftest)//2)])
+	    initialsentiment = textblob_sentiment(firsthalf)
+	    firstsample.append(initialsentiment[0])
+	    firstsample.append(initialsentiment[1])
+	    endingsentiment = textblob_sentiment(secondhalf)
+	    secondsample.append(endingsentiment[0])
+	    secondsample.append(endingsentiment[1])
+	    firsttoken = nltk.word_tokenize(firsthalf)
+	    secondtoken = nltk.word_tokenize(secondhalf)
+	    firstrating = warriner_rating(firsttoken)
+	    firstsample += firstrating
+	    secondrating = warriner_rating(secondtoken)
+	    secondsample += secondrating
 
-#data.append(sample)
-#print(data)
+	    difference = [0]*len(firstsample)
+	    for i in range(len(firstsample)):
+	    	 difference[i] = firstsample[i] - secondsample[i]
+
+	    
+	    last10lines = "\n".join(text.split("\n")[-10:])
+
+
+
+	    sentiment = textblob_sentiment(last10lines) #or text
+	    sample = []
+	    sample.append(sentiment[0])
+	    sample.append(sentiment[1])
+	    alltokens = nltk.word_tokenize(last10lines) #or text
+	    ratings = warriner_rating(alltokens)
+	    sample += ratings
+	    #trainingdata.append(sample)
+	    trainingdata.append(difference)
+	    f.close()
+	    
+
+
+def calculate_avg_score(data):
+	numOfScript = 0
+	avg_score = [0,0,0,0,0,0,0,0]
+	for d in data:
+		numOfScript += 1
+		for i in range(len(avg_score)):
+			avg_score[i] += d[i]
+	for j in range(len(avg_score)):
+		avg_score[j] /= numOfScript
+	avg_score = [round(x,5) for x in avg_score]
+	return avg_score
+
+
+
+####################################
+analyze_script("bad", baddata)
+analyze_script("good", gooddata)
+
+bad_avg_score = calculate_avg_score(baddata)
+good_avg_score = calculate_avg_score(gooddata)
+
+
+print("avg score for bad:", bad_avg_score)
+print("avg score for good:", good_avg_score)
+
+
+for data in gooddata:
+	data.append(1)
+for data in baddata:
+	data.append(0)
+
+data = gooddata + baddata
+dataset = np.array(data)
+
+
+#print(dataset)
+
+ 
+
+
+#######MACHINE LEARNING MODEL#############
+
+from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import cross_validate
+from sklearn import metrics
+
+
+from sklearn import svm, datasets
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+
+def classify(dataset):
+	(numSamples, numFeatures) = dataset.shape
+	data = dataset[:,range(numFeatures-1)].reshape((numSamples, numFeatures-1))
+	labels = dataset[:, numFeatures-1].reshape((numSamples,))
+	(numSamples, numFeatures) = data.shape
+	print(labels)
+	print(len(labels))
+	print(sum(labels))
+	print("majority baseline:", sum(labels)/len(labels))
+
+
+	print(" ")
+	print(" ")
+	gnb = GaussianNB()
+	scoring_metrics = ['accuracy', 'precision', 'recall', 'f1']
+	# Let's train a model with 5-fold cross validation.
+	scores = cross_validate(gnb, data, labels, cv=5, scoring=scoring_metrics)
+	# Then we print out each of the metrics for each of the 5 folds.
+	for score_name, score_value in scores.items():
+	    print(score_name, score_value.mean())
+	print(" ")
+	print(" ")
+	from sklearn.neighbors import KNeighborsClassifier
+	neigh = KNeighborsClassifier(n_neighbors=3)
+	knn_scores = cross_validate(neigh, data, labels, cv=5, scoring= scoring_metrics)
+	for score_name, score_value in knn_scores.items():
+	    print(score_name, score_value.mean())
+	print(" ")
+	print(" ")
+
+	# randomly select a subset of your data (size = 20)
+	testid = [1, 1]
+	while len(testid) != len(set(testid)):
+	    testid = np.random.randint(0, data.shape[0], 20)
+	print(f'len(testid) = {len(testid)}')
+	print(f'len(set(testid)) = {len(set(testid))}')
+	# Get your testing data
+	print(testid)
+	testset = data[testid, :]
+	testtarget = labels[testid]
+	print(testset.shape)
+	# Get your training data
+	trainset = np.delete(data, testid, 0)
+	traintarget = np.delete(labels, testid, 0)
+	print(trainset.shape)
+	# Build model using fit()
+	model = GaussianNB()
+	model.fit(trainset, traintarget)
+	# Apply model to test set using predict()
+	expected = testtarget
+	predicted = model.predict(testset)
+	# Print a classification report
+	print(metrics.classification_report(expected, predicted))
+	# Print a confusion matrix (true positive, falsepositives, etc.)
+	print(metrics.confusion_matrix(expected, predicted))
+
+
+
+
+classify(dataset)
+
+
+####PLOTTTING STUFF #######
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+gooddata = np.array(gooddata)
+baddata = np.array(baddata)
+
+goodArousal = gooddata[:,2]
+goodValence = gooddata[:,3]
+
+badArousal = baddata[:,2]
+badValence = baddata[:,3]
+
+plt.scatter(goodArousal, goodValence)
+plt.scatter(badArousal, badValence)
+plt.show()
 
 
 
